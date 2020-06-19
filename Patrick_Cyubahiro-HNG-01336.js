35525781f53bcd3ea726cb13f28e47ac80a0d780
Patrick CYUBAHIRO <ampatrickcyubahiro@gmail.com>
2020-06-19 19:28:54 +0300

    Update Patrick_Cyubahiro-HNG-01336.js

--- Patrick_Cyubahiro-HNG-01336.js
+++ Patrick_Cyubahiro-HNG-01336.js
@@ -1,5 +1,5 @@
 exports.findById = [
-con.query("SELECT mda_name, expense, date FROM DATABASE_NAME", function (err, result, fields)) {
+con.query("SELECT mda_name, expense, date FROM DATABASE_NAME", function (err, mda_name)) {
 if (err) throw err;
 console.log('The mda name that contracted the project:');
 console.log(mda_name);
